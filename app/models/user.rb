b53460102d3dff5d9643f25bcbc5bdaf085de186
nema <neman.gogo@gmail.com>
2016-09-22 17:54:23 +0900

    AvatarUploaderの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name, :member, :profile, :works
+
+  mount_uploader :avatar, AvatarUploader
 end
