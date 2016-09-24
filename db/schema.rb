080f4cf9a85c6b1fc021d2b8c72c08cb46c2729c
nema <neman.gogo@gmail.com>
2016-09-24 13:21:29 +0900

    captured imagesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922082220) do
+ActiveRecord::Schema.define(version: 20160923070954) do
+
+  create_table "capturedimages", force: :cascade do |t|
+    t.integer  "prototype_id", limit: 4
+    t.string   "image",        limit: 255
+    t.integer  "role",         limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",      limit: 255
