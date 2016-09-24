64df48525e9df8a6f0adf667ca23181c0c1cb428
nema <neman.gogo@gmail.com>
2016-09-24 13:15:06 +0900

    アソシエーションの設定。

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,8 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
+  has_many :capturedimages
+
+  accepts_nested_attributes_for :capturedimages
+
+  validates_presence_of :title, :catchcopy, :concept
 end
