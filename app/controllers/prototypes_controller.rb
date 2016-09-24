cf2a1216e3cf8540f2ae5ef1ee894946555bc001
nema <neman.gogo@gmail.com>
2016-09-24 13:11:01 +0900

    コントローラーの編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
 
   def index
-
   end
 
   def show
@@ -9,6 +8,24 @@ class PrototypesController < ApplicationController
   end
 
   def new
-
+    @prototype = Prototype.new
+    @prototype.capturedimages.build
   end
+
+  def create
+    @prototype = Prototype.create(create_params)
+    if @prototype.errors.present?
+      flash[:error] = @prototype.errors.full_messages
+      redirect_to prototypes_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:prototype).permit(:title, :catchcopy, :concept, capturedimages_attributes: [:prototype_id, :image, :role]).merge(user_id: current_user.id)
+  end
+
 end
