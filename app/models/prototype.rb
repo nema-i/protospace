ac405140b8d6a5dac477b097eacaec4f77e6d3d0
nema <neman.gogo@gmail.com>
2016-09-29 14:40:29 +0900

    プロトタイプが削除されたら、紐づくcapturedimagesも削除。

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :capturedimages
+  has_many :capturedimages, dependent: :delete_all
 
   accepts_nested_attributes_for :capturedimages
 
