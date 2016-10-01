b2a76f4930a8d5ba343e7045900122d148795798
nema <neman.gogo@gmail.com>
2016-10-01 12:14:32 +0900

    関連するモデルの削除をする為に、allow_destroy: trueを追記

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :capturedimages, dependent: :delete_all
 
-  accepts_nested_attributes_for :capturedimages
+  accepts_nested_attributes_for :capturedimages, allow_destroy: true
 
   validates_presence_of :title, :catchcopy, :concept
 end
