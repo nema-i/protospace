89043802688fc78f210d95cc63c3d7babadc98a5
nema <neman.gogo@gmail.com>
2016-10-04 20:53:57 +0900

    likesコントローラーの編集。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,11 @@
 class LikesController < ApplicationController
   def create
-    Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
+    Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 
   def destroy
-    Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id]).destroy
-    @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
+    @prototype.like_user(current_user.id).destroy
   end
 end
